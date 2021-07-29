c10ee691f1f9bed5b1c49395333ed7e905fd25b4
Ryely <ryely.moore@outlook.com>
2021-07-29 14:13:43 -0600

    fill in server.js to create a connection to user_db

---
+++ server.js
@@ -0,0 +1,17 @@
+// requires express.js package and the created sequelize const from our connection.js file
+const express = require('express');
+const sequelize = require('./config/connection');
+
+// assigns an instance of express to app
+const app = express();
+// assigns a value to PORT relative to the environment that the code is being run in, if no specific env is found sets PORT to 3001
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now Listening'));
+});
